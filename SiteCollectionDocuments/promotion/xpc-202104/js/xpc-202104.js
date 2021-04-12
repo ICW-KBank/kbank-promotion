f93a3e35d2aff7ed513ff1ea16188dc27c2b1677
Bluenarysun <tee.emilond@gmail.com>
2021-04-13 00:03:26 +0700

    Animate git on table done.

--- SiteCollectionDocuments/promotion/xpc-202104/js/xpc-202104.js
+++ SiteCollectionDocuments/promotion/xpc-202104/js/xpc-202104.js
@@ -1,49 +1,36 @@
-"use strict";
+'use strict';
 $(function () {
-  
-  $(".open-promotion-table").ready(function () {
-    $(".open-promotion-table").magnificPopup({
-      type: "inline",
-      mainClass: "promotion-popup theme-home-loan",
-      overflowY: "scroll",
-      closeMarkup: '<button type="button" class="mfp-close"></button>',
-      autoFocusLast: false,
-      fixedContentPos: true,
-      callbacks: {
-        open: function () {
-          $(window).trigger("resize");
-        }
+  if ($('.table-anime').length) {
+    $('.table-anime').on('click', function (e) {
+      e.preventDefault();
+      $(this).css('display', 'none');
+    });
+  }
+
+  if ($('.accordion').length) {
+    $('.accordion').each(function () {
+      var acc = $(this),
+        title = $(this).find('>.accordion-title'),
+        content = $(this).find('>.accordion-content');
+
+      content.hide();
+
+      if (acc.find('.accordion-content').length == 0) {
+        acc.addClass('empty');
       }
-    });
-  });
 
-  if ($(".accordion").length) {
-    $(".accordion").each(function() {
-        var acc = $(this),
-            title = $(this).find(">.accordion-title"),
-            content = $(this).find(">.accordion-content"); 
+      if (title.hasClass('active')) {
+        $(this).parent().find(content).slideDown(300);
+      }
 
-        content.hide();
-
-        if(acc.find(".accordion-content").length == 0) {
-            acc.addClass("empty");
+      title.click(function () {
+        if ($(this).hasClass('active')) {
+          $(this).removeClass('active').parent().find(content).slideUp(300);
+        } else {
+          $(this).toggleClass('active').parent().find(content).slideDown(300);
         }
-
-        if(title.hasClass('active')) {
-            $(this).parent().find(content).slideDown(300);
-        }
-
-        title.click(function() {
-            if ($(this).hasClass('active')) {
-               $(this).removeClass('active').parent().find(content).slideUp(300);
-            } else {
-               $(this).toggleClass('active').parent().find(content).slideDown(300);
-            }
-            return false;
-        });
+        return false;
+      });
     });
-}
-  
-  
+  }
 });
-
